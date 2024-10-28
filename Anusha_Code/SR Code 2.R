e3710a657233b3c8bad40439480f0ec4b7c60059
anusha-akhtar <aakhtar0323@gmail.com>
2024-10-28 14:59:12 -0500

    clinvar_work

--- Anusha_Code/SR Code 2.R
+++ Anusha_Code/SR Code 2.R
@@ -1,4 +1,5 @@
 exonfile <- 'C:/Users/sramachandran/Documents/GitHub/Capstone_2024/Resources/CodingLocs_hg38.rds'
+exonfile <- '/Users/anushaakhtar/Documents/GitHub/Capstone_2024/Resources/CodingLocs_hg38.rds'
 readRDS(exonfile)
 exonpositions <- readRDS(exonfile)
 
@@ -26,10 +27,12 @@ library(dplyr)
 
 # Define Clinvar path to VCF 
 clinvar_vcf <- "F:/Capstone/Resources/ClinVar/clinvar.vcf.gz"
+clinvar_vcf <- "/Volumes/Seagate/Capstone/clinvar_20241027.vcf"
 
 # Load the VCF file
 #change to 38?
 vcf_clinvar <- readVcf(clinvar_vcf, "hg38") 
+dim(clinvar_vcf)
 
 # Check the contents of the VCF file
 vcf_clinvar
@@ -44,9 +47,119 @@ fixed(vcf_clinvar)
 # Check the INFO fields (annotations like clinical significance, allele frequency, etc.)
 info(vcf_clinvar)
 
+# Check the INFO fields (annotations like clinical significance, allele frequency, etc.)
+info_data <- info(vcf_clinvar)
+info_data
+clinvar_metadata_df <- as.data.frame(info_data)
+
+
+# Extract the genomic ranges and convert to data frame
+genomic_ranges <- rowRanges(vcf_clinvar)
+clinvar_gr <- as.data.frame(genomic_ranges)
+
+# Extract fixed data into a dataframe
+fixed_data <- data.frame(
+  CHROM = seqnames(genomic_ranges),
+  POS = start(genomic_ranges),
+  REF = fixed(vcf_clinvar)$REF,
+  ALT = fixed(vcf_clinvar)$ALT
+)
+
+# Combine fixed and info data into a single data frame
+clinvar_df <- cbind(fixed_data, info_data)
+
+# Selecting specific columns 
+selected_columns <- clinvar_df %>%
+  select(CHROM, POS, REF, ALT, AF_ESP, CLNSIG)
+
+# View the new dataset
+head(selected_columns)
+
+
+
+
 # Just try on CHR 22 for workflow 
 ## Path to the gnomAD VCF for chromosome 22
 gnomad_vcf_chr22 <- "F:/Capstone/Resources/gnomAD/gnomad.joint.v4.1.sites.chr22.vcf.bgz"
+
+rng <- GRanges(seqnames="chr22", ranges=IRanges(
+  start=c(50301422, 50989541), 
+  end=c(50312106, 51001328),
+  names=c("gene_79087", "gene_644186")))
+
+tab <- TabixFile(gnomad_vcf_chr22)
+vcf_rng <- readVcf(tab, "hg38", param=rng)
+
+
+# We would want to exlude everything in the filter column that doesn't say PASS
+info(vcf_rng)
+
+header(vcf_rng)
+
+# row ranges to find the position and other fixed things we will need to keep and the filter info 
+head(rowRanges(vcf_rng), 3)
+
+# 
+A <- info(header(vcf_rng))
+write.table(file="header.txt",A, quote= FALSE, sep="\t")
+
+# Load the gnomAD VCF file for chromosome 22
+vcf_gnomad_chr22 <- readVcf(gnomad_vcf_chr22, "hg38")
+
+# Check the contents
+head(vcf_gnomad_chr22)
+
+# Look for the what the fixed fields are 
+fixed_fields <- fixed(vcf_gnomad_chr22)
+
+# Filter the gnomAD VCF for this region
+filtered_gnomad_chr22 <- subsetByOverlaps(vcf_gnomad_chr22, regions)
+
+# Filter the ClinVar VCF for the same region
+filtered_clinvar <- subsetByOverlaps(vcf_clinvar, regions)
+
+# Convert filtered gnomAD and ClinVar VCFs to data frames
+gnomad_df <- as.data.frame(filtered_gnomad_chr22)
+clinvar_df <- as.data.frame(filtered_clinvar)
+
+### Oct 25 
+# Look for fixed fields within GnomAD
+fixed_fields <- fixed(vcf_rng)
+head(fixed_fields)
+
+# Check the info fields 
+info_fields <- info(header(vcf_rng))
+info_fields
+
+# Since we couldnt not fine CHR or POS in fixed file - use row ranges 
+head(rowRanges(vcf_rng))
+
+# Convert rowRanges to a data frame to extract CHROM and POS
+chrom_pos_df <- as.data.frame(rowRanges(vcf_rng))[, c("seqnames", "start", "REF", "ALT")]
+
+# Rename the columns so POS and CHROM are clear 
+chrom_pos_df <- chrom_pos_df %>%
+  rename(
+    CHROM = seqnames,
+    POS = start
+  )
+
+# Review DF 
+head(chrom_pos_df)
+
+# Extract the FAFmax_faf95_max_joint column from the INFO fields
+fafmax <- info(vcf_rng)$FAFmax_faf95_max_joint
+
+# Add the INFO field to chrom_pos_df
+chrom_pos_df$FAFmax_faf95_max_joint <- fafmax
+
+#View updated DF
+head(chrom_pos_df)
+
+# Just try on CHR 22 for workflow 
+## Path to the gnomAD VCF for chromosome 22
+gnomad_vcf_chr22 <- "F:/Capstone/Resources/gnomAD/gnomad.joint.v4.1.sites.chr22.vcf.bgz"
+gnomad_vcf_chr22 <- "/Volumes/Seagate/Capstone/gnomad.joint.v4.1.sites.chr22.vcf.bgz"
 
 rng <- GRanges(seqnames="chr22", ranges=IRanges(
   start=c(50301422, 50989541), 
